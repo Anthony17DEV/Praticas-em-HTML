46e51b93a75786b31c6c0547fd618666a853e6a7
Anthony17DEV <109322252+Anthony17DEV@users.noreply.github.com>
2022-08-08 21:45:35 -0300

    Create urlpost.php

---
+++ urlpost.php
@@ -0,0 +1,14 @@
+<?php
+
+switch($_POST["fruta"]){
+  case "maca":
+    echo "<img src='https://icons.iconarchive.com/icons/alex-t/splash-of-fruit/96/button-apple-icon.png'>";
+    break;
+  case "limão":
+    echo "<img src='https://icons.iconarchive.com/icons/alex-t/splash-of-fruit/96/button-lemon-icon.png'>";
+    break;
+  case "uva":
+    echo "<img src='https://icons.iconarchive.com/icons/alex-t/splash-of-fruit/96/button-grape-icon.png'>";
+    break;
+  
+}
